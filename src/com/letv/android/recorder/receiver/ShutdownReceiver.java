64d34bd0d3d28aad01ee10e93ce4143968848238
wuwen <wuwen@letv.com>
2014-11-21 14:26:56 +0800

    module: Recorder submodule: none
    Message Content: fix bug LEUI-5059
    
    Ticket: LEUI-5059
    
    Change-Id: Ib8a216fb5688d9b229eb700ac1905df50391cdd4
    Signed-off-by: wuwen <wuwen@letv.com>

---
+++ src/com/letv/android/recorder/receiver/ShutdownReceiver.java
@@ -0,0 +1,26 @@
+package com.letv.android.recorder.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import com.letv.android.recorder.service.Recorder;
+import com.letv.android.recorder.service.RecorderService;
+import com.letv.android.recorder.tool.RecordTool;
+
+/**
+ * Created by snile on 14/11/21.
+ */
+public class ShutdownReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Recorder.MediaRecorderState state = RecordTool.getRecordState(context);
+        Log.e("--------","ShutdownReceiver");
+        if(state == Recorder.MediaRecorderState.PAUSED||
+                state == Recorder.MediaRecorderState.RECORDING){
+            RecorderService.stopRecording(context);
+            RecorderService.saveRecording(context,RecordTool.getRecordName(context));
+        }
+    }
+}
