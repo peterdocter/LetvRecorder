ca02630b12a5e21fb86d7be7cc0101c525a9757d
cuiguoqiang <cuiguoqiang@letv.com>
2015-04-07 20:48:55 +0800

    132-LetvRecorder: add international String
    add String in values-en
    fix AlertDialog Style
    
    Ticket: SI-7030
    
    Change-Id: I5628f7f071fd9005756f6f355f950fe1fc1d44be
    Signed-off-by: cuiguoqiang <cuiguoqiang@letv.com>

--- src/com/letv/android/recorder/widget/EditRecordNameDialog.java
+++ src/com/letv/android/recorder/widget/EditRecordNameDialog.java
@@ -35,7 +35,7 @@ public class EditRecordNameDialog {
         builder.setTitle(R.string.edit_recorder_name)
                 .setView(mEditText);
         builder.setPositiveButtonPattern(AlertDialog.ButtonPattern.EXPECTATION)// 期望，蓝色
-                .setNeutralButtonPattern(AlertDialog.ButtonPattern.WARNING);//　警告，红色
+                .setNeutralButtonPattern(AlertDialog.ButtonPattern.NORMAL);//　警告，红色
         mEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(50)});
 
     }
